ee2625180fa5da5be1c9a0107a0c2a681d7b5439
akhilkumar-vemula <akhilkumar.vemula@tekreant.com>
2025-05-28 14:15:03 +0530

    dist files moved

---
+++ .next/static/chunks/app/layout-82733932166f5635.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7807:(e,s,_)=>{Promise.resolve().then(_.bind(_,5555))}},e=>{var s=s=>e(e.s=s);e.O(0,[920,555,441,684,358],()=>s(7807)),_N_E=e.O()}]);
